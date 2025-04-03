cf0fae055cfd58165d8936c4d517b9c9e1422a9f
Takathedinosaur <takathedinosaur@gmail.com>
2025-04-03 21:00:36 +0200

    Fix some of the comments and formatting

--- main.ts
+++ main.ts
@@ -1,47 +1,50 @@
-import {OBSRequestTypes, OBSWebSocket} from "npm:obs-websocket-js"
+import { OBSRequestTypes, OBSWebSocket } from "npm:obs-websocket-js";
 import { obs } from "./private_config.ts";
 
 const obsClient = new OBSWebSocket();
 
-await obsClient.connect("ws://" + obs.ip + ":" + obs.port, obs.password)
+await obsClient.connect("ws://" + obs.ip + ":" + obs.port, obs.password);
 
-const currentScene = await obsClient.call('GetCurrentProgramScene');
+const currentScene = await obsClient.call("GetCurrentProgramScene");
 
-const get_item_id: OBSRequestTypes['GetSceneItemId'] = {
+const get_item_id: OBSRequestTypes["GetSceneItemId"] = {
   sourceName: obs.scene_name,
-  sceneUuid: currentScene.sceneUuid
-}
+  sceneUuid: currentScene.sceneUuid,
+};
 
-const {sceneItemId} = await obsClient.call('GetSceneItemId',get_item_id);
+const { sceneItemId } = await obsClient.call("GetSceneItemId", get_item_id);
 
-const get_item_transform: OBSRequestTypes['GetSceneItemTransform'] = {
+const get_item_transform: OBSRequestTypes["GetSceneItemTransform"] = {
   sceneItemId,
-  sceneUuid: currentScene.sceneUuid
-}
+  sceneUuid: currentScene.sceneUuid,
+};
 
 type SceneItemTransform = {
-  alignment: number,                                                                                                         
-  boundsAlignment: number,                                                                                                   
-  boundsHeight: number,                                                                                                      
-  boundsType: "OBS_BOUNDS_NONE",
-  boundsWidth: number,
-  cropBottom: number,
-  cropLeft: number,
-  cropRight: number,
-  cropToBounds: false,
-  cropTop: number,
-  height: number,
-  positionX: number,
-  positionY: number,
-  rotation: number,
-  scaleX: number,
-  scaleY: number,
-  sourceHeight: number,
-  sourceWidth: number,
-  width: number
-}
+  alignment: number;
+  boundsAlignment: number;
+  boundsHeight: number;
+  boundsType: "OBS_BOUNDS_NONE";
+  boundsWidth: number;
+  cropBottom: number;
+  cropLeft: number;
+  cropRight: number;
+  cropToBounds: false;
+  cropTop: number;
+  height: number;
+  positionX: number;
+  positionY: number;
+  rotation: number;
+  scaleX: number;
+  scaleY: number;
+  sourceHeight: number;
+  sourceWidth: number;
+  width: number;
+};
 
-const {sceneItemTransform} = await obsClient.call('GetSceneItemTransform', get_item_transform);
+const { sceneItemTransform } = await obsClient.call(
+  "GetSceneItemTransform",
+  get_item_transform,
+);
 
 let transform: SceneItemTransform = sceneItemTransform as SceneItemTransform;
 
@@ -51,18 +54,18 @@ export async function reset() {
   transform.scaleY = 1;
   transform.alignment = 0;
 
-  const req: OBSRequestTypes['SetSceneItemTransform'] = {
+  const req: OBSRequestTypes["SetSceneItemTransform"] = {
     sceneItemId,
     sceneUuid: currentScene.sceneUuid,
     sceneItemTransform: {
       rotation: transform.rotation,
       scaleX: transform.scaleX,
       scaleY: transform.scaleY,
-      alignment: 0
-    }
+      alignment: 0,
+    },
   };
 
-  await obsClient.call('SetSceneItemTransform', req);
+  await obsClient.call("SetSceneItemTransform", req);
 }
 
 export async function rotate(unit: number) {
@@ -72,44 +75,58 @@ export async function rotate(unit: number) {
   const w = 960, h = 540;
 
   const r = {
-    top_left:    [Math.abs(rectangle.top_left[0] / w), Math.abs(rectangle.top_left[1] / h)],
-    top_right:   [Math.abs(rectangle.top_right[0] / w), Math.abs(rectangle.top_right[1] / h)],
-    bottom_left: [Math.abs(rectangle.bottom_left[0] / w), Math.abs(rectangle.bottom_left[1] / h)],
-    bottom_right:[Math.abs(rectangle.bottom_right[0] / w), Math.abs(rectangle.bottom_right[1] / h)]
-  }
+    top_left: [
+      Math.abs(rectangle.top_left[0] / w),
+      Math.abs(rectangle.top_left[1] / h),
+    ],
+    top_right: [
+      Math.abs(rectangle.top_right[0] / w),
+      Math.abs(rectangle.top_right[1] / h),
+    ],
+    bottom_left: [
+      Math.abs(rectangle.bottom_left[0] / w),
+      Math.abs(rectangle.bottom_left[1] / h),
+    ],
+    bottom_right: [
+      Math.abs(rectangle.bottom_right[0] / w),
+      Math.abs(rectangle.bottom_right[1] / h),
+    ],
+  };
 
-  const x_max = Math.max(r.top_left[0], r.top_right[0], r.bottom_left[0], r.bottom_right[0]);
-  const y_max = Math.max(r.top_left[1], r.top_right[1], r.bottom_left[1], r.bottom_right[1]);
+  const x_max = Math.max(
+    r.top_left[0],
+    r.top_right[0],
+    r.bottom_left[0],
+    r.bottom_right[0],
+  );
+  const y_max = Math.max(
+    r.top_left[1],
+    r.top_right[1],
+    r.bottom_left[1],
+    r.bottom_right[1],
+  );
 
   const max = Math.max(x_max, y_max);
 
   console.log(max);
 
-
-  
-  const req: OBSRequestTypes['SetSceneItemTransform'] = {
+  const req: OBSRequestTypes["SetSceneItemTransform"] = {
     sceneItemId,
     sceneUuid: currentScene.sceneUuid,
     sceneItemTransform: {
       rotation: transform.rotation,
       scaleX: 1 / max,
       scaleY: 1 / max,
-    }
+    },
   };
 
-  await obsClient.call('SetSceneItemTransform', req);
-
-
-
+  await obsClient.call("SetSceneItemTransform", req);
 
   transform = sceneItemTransform as SceneItemTransform;
 }
 
-
-
 // /* 0, 0, 1920, 1080 => rotate thanks to the angle => calculate the cosine and sine functions and
 // DO something cool !!
-
 
 //   0,0 --------------------- 1920,0
 //       |                   |
@@ -118,41 +135,43 @@ export async function rotate(unit: number) {
 //       ---------------------
 //   0,1080                    1920, 1080
 
-
-//  yvelocity += gravity  
+//  yvelocity += gravity
 
 // */
 
-
+/// ASSUMES OBS IS RUNNING IN 1920x1080!!!!!
 const rect = {
-  top_left: [-960, -540], 
-  top_right: [960, -540], 
-  bottom_left: [-960, 540], 
-  bottom_right: [960, 540]
-}
+  top_left: [-960, -540],
+  top_right: [960, -540],
+  bottom_left: [-960, 540],
+  bottom_right: [960, 540],
+};
 
 function rotate_by_angle(angle: number) {
   return {
     top_left: [
       rect.top_left[0] * Math.cos(angle) - rect.top_left[1] * Math.sin(angle),
       rect.top_left[0] * Math.sin(angle) + rect.top_left[1] * Math.cos(angle),
-    ], 
+    ],
     top_right: [
       rect.top_right[0] * Math.cos(angle) - rect.top_right[1] * Math.sin(angle),
       rect.top_right[0] * Math.sin(angle) + rect.top_right[1] * Math.cos(angle),
-    ], 
+    ],
     bottom_left: [
-      rect.bottom_left[0] * Math.cos(angle) - rect.bottom_left[1] * Math.sin(angle),
-      rect.bottom_left[0] * Math.sin(angle) + rect.bottom_left[1] * Math.cos(angle),
-    ], 
+      rect.bottom_left[0] * Math.cos(angle) -
+      rect.bottom_left[1] * Math.sin(angle),
+      rect.bottom_left[0] * Math.sin(angle) +
+      rect.bottom_left[1] * Math.cos(angle),
+    ],
     bottom_right: [
-      rect.bottom_right[0] * Math.cos(angle) - rect.bottom_right[1] * Math.sin(angle),
-      rect.bottom_right[0] * Math.sin(angle) + rect.bottom_right[1] * Math.cos(angle),
-    ]
-  }
+      rect.bottom_right[0] * Math.cos(angle) -
+      rect.bottom_right[1] * Math.sin(angle),
+      rect.bottom_right[0] * Math.sin(angle) +
+      rect.bottom_right[1] * Math.cos(angle),
+    ],
+  };
 }
 
-
 function degreeToRadians(degrees: number) {
-  return (degrees) * Math.PI / 180.0;
+  return degrees * Math.PI / 180.0;
 }
--- twitch_bot_example.ts
+++ twitch_bot_example.ts
@@ -1,183 +1,203 @@
-
 import process from "node:process";
 
- 
-import {twitch} from "./private_config.ts"
+import { twitch } from "./private_config.ts";
 import { reset, rotate } from "./main.ts";
-const OAUTH_TOKEN = twitch.access_token; // Needs scopes user:bot, user:read:chat, user:write:chat
+const OAUTH_TOKEN = twitch.access_token; // Needs channel:read:redemptions, user:read:chat, 
 const CLIENT_ID = twitch.client_id;
 
-let CHAT_CHANNEL_USER_ID:string = twitch.channel_id; // This is the User ID of the channel that the bot will join and listen to chat messages of
+let CHAT_CHANNEL_USER_ID: string = twitch.channel_id; // This is the User ID of the channel that the bot will join and listen to chat messages of
 
-const EVENTSUB_WEBSOCKET_URL = 'wss://eventsub.wss.twitch.tv/ws';
+const EVENTSUB_WEBSOCKET_URL = "wss://eventsub.wss.twitch.tv/ws";
 
 let websocketSessionID: string;
 
 // Start executing the bot from here
 (async () => {
-	// Verify that the authentication is valid
-	await getAuth();
+  // Verify that the authentication is valid
+  await getAuth();
 
-	// Start WebSocket client and register handlers
-	const _websocketClient = startWebSocketClient();
+  // Start WebSocket client and register handlers
+  const _websocketClient = startWebSocketClient();
 })();
 
 // WebSocket will persist the application loop until you exit the program forcefully
 
 async function getAuth() {
-	// https://dev.twitch.tv/docs/authentication/validate-tokens/#how-to-validate-a-token
-	const response = await fetch('https://id.twitch.tv/oauth2/validate', {
-		method: 'GET',
-		headers: {
-			'Authorization': 'OAuth ' + OAUTH_TOKEN
-		}
-	});
+  // https://dev.twitch.tv/docs/authentication/validate-tokens/#how-to-validate-a-token
+  const response = await fetch("https://id.twitch.tv/oauth2/validate", {
+    method: "GET",
+    headers: {
+      "Authorization": "OAuth " + OAUTH_TOKEN,
+    },
+  });
 
-	if (response.status != 200) {
-		const data = await response.json();
-		console.error("Token is not valid. /oauth2/validate returned status code " + response.status);
-		console.error(data);
-		process.exit(1);
-	}
+  if (response.status != 200) {
+    const data = await response.json();
+    console.error(
+      "Token is not valid. /oauth2/validate returned status code " +
+        response.status,
+    );
+    console.error(data);
+    process.exit(1);
+  }
 
-    const user_response = await fetch("https://api.twitch.tv/helix/users?login=" + twitch.channel_name, {
-        method: "GET", 
-        headers: {
-            Authorization: "Bearer " + OAUTH_TOKEN,
-            "Client-Id": CLIENT_ID,
-        }
-    }).then(r => r.json());
+  const user_response = await fetch(
+    "https://api.twitch.tv/helix/users?login=" + twitch.channel_name,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + OAUTH_TOKEN,
+        "Client-Id": CLIENT_ID,
+      },
+    },
+  ).then((r) => r.json());
 
-    CHAT_CHANNEL_USER_ID = user_response.data[0].id;
-    console.log("CHAT CHANNEL USER ID: " + CHAT_CHANNEL_USER_ID);
+  CHAT_CHANNEL_USER_ID = user_response.data[0].id;
+  console.log("CHAT CHANNEL USER ID: " + CHAT_CHANNEL_USER_ID);
 
-
-	console.log("Validated token.");
+  console.log("Validated token.");
 }
 
 function startWebSocketClient() {
-	const websocketClient = new WebSocket(EVENTSUB_WEBSOCKET_URL);
+  const websocketClient = new WebSocket(EVENTSUB_WEBSOCKET_URL);
 
-	websocketClient.onerror = console.error;
+  websocketClient.onerror = console.error;
 
-	websocketClient.onopen = () => {
-		console.log('WebSocket connection opened to ' + EVENTSUB_WEBSOCKET_URL);
-	};
+  websocketClient.onopen = () => {
+    console.log("WebSocket connection opened to " + EVENTSUB_WEBSOCKET_URL);
+  };
 
-	websocketClient.onmessage = (data) => {
-		handleWebSocketMessage(data);
-	};
+  websocketClient.onmessage = (data) => {
+    handleWebSocketMessage(data);
+  };
 
-	return websocketClient;
+  return websocketClient;
 }
 
 // deno-lint-ignore no-explicit-any
-async function handleWebSocketMessage({data: str}: MessageEvent<any>) {
-    const data = JSON.parse(str);
-	switch (data.metadata.message_type) {
-		case 'session_welcome': // First message you get from the WebSocket server when connecting
-			websocketSessionID = data.payload.session.id; // Register the Session ID it gives us
+async function handleWebSocketMessage({ data: str }: MessageEvent<any>) {
+  const data = JSON.parse(str);
+  switch (data.metadata.message_type) {
+    case "session_welcome": // First message you get from the WebSocket server when connecting
+      websocketSessionID = data.payload.session.id; // Register the Session ID it gives us
 
-			// Listen to EventSub, which joins the chatroom from your bot's account
-			registerEventSubListeners();
-			break;
-		case 'notification': // An EventSub notification has occurred, such as channel.chat.message
-			switch (data.metadata.subscription_type) {
-				case 'channel.chat.message':
-					if(data.payload.event.chatter_user_id == CHAT_CHANNEL_USER_ID && data.payload.event.message.text.includes("!reset")) {
-						await reset();
-					}
-					break;
-				case 'channel.channel_points_custom_reward_redemption.add':
-                    if(data.payload.event.reward.id == twitch.clockwise_event) {
-                        await rotateClockwise()
-                    }
-                    else if(data.payload.event.reward.id == twitch.counterclockwise_event) {
-						await rotateCounterClockwise()
-                    }
-                    break;
-			}
-			break;
-	}
+      // Listen to EventSub, which joins the chatroom from your bot's account
+      registerEventSubListeners();
+      break;
+    case "notification": // An EventSub notification has occurred, such as channel.chat.message
+      switch (data.metadata.subscription_type) {
+        case "channel.chat.message":
+          if (
+            data.payload.event.chatter_user_id == CHAT_CHANNEL_USER_ID &&
+            data.payload.event.message.text.includes("!reset")
+          ) {
+            await reset();
+          }
+          break;
+        case "channel.channel_points_custom_reward_redemption.add":
+          console.log(data.payload);
+          if (data.payload.event.reward.id == twitch.clockwise_event) {
+            await rotateClockwise();
+          } else if (
+            data.payload.event.reward.id == twitch.counterclockwise_event
+          ) {
+            await rotateCounterClockwise();
+          }
+          break;
+      }
+      break;
+  }
 }
-
-
 
 async function registerEventSubListeners() {
-	{
-	
-		// Register channel.chat.message
-		const response = await fetch('https://api.twitch.tv/helix/eventsub/subscriptions', {
-			method: 'POST',
-			headers: {
-				'Authorization': 'Bearer ' + OAUTH_TOKEN,
-				'Client-Id': CLIENT_ID,
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				type: 'channel.channel_points_custom_reward_redemption.add',
-				version: '1',
-				condition: {
-					broadcaster_user_id: CHAT_CHANNEL_USER_ID,
-				},
-				transport: {
-					method: 'websocket',
-					session_id: websocketSessionID
-				}
-			})
-		});
+  {
+    // Register channel.channel_points_custom_reward_redemption.add
+    const response = await fetch(
+      "https://api.twitch.tv/helix/eventsub/subscriptions",
+      {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + OAUTH_TOKEN,
+          "Client-Id": CLIENT_ID,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          type: "channel.channel_points_custom_reward_redemption.add",
+          version: "1",
+          condition: {
+            broadcaster_user_id: CHAT_CHANNEL_USER_ID,
+          },
+          transport: {
+            method: "websocket",
+            session_id: websocketSessionID,
+          },
+        }),
+      },
+    );
 
-		if (response.status != 202) {
-			const data = await response.json();
-			console.error("Failed to subscribe to channel.channel_points_custom_reward_redemption.add. API call returned status code " + response.status);
-			console.error(data);
-			process.exit(1);
-		} else {
-			const data = await response.json();
-			console.log(`Subscribed to channel.channel_points_custom_reward_redemption.add [${data.data[0].id}]`);
-		}
-	}
+    if (response.status != 202) {
+      const data = await response.json();
+      console.error(
+        "Failed to subscribe to channel.channel_points_custom_reward_redemption.add. API call returned status code " +
+          response.status,
+      );
+      console.error(data);
+      process.exit(1);
+    } else {
+      const data = await response.json();
+      console.log(
+        `Subscribed to channel.channel_points_custom_reward_redemption.add [${
+          data.data[0].id
+        }]`,
+      );
+    }
+  }
 
-	{
-				// Register channel.chat.message
-				const response = await fetch('https://api.twitch.tv/helix/eventsub/subscriptions', {
-					method: 'POST',
-					headers: {
-						'Authorization': 'Bearer ' + OAUTH_TOKEN,
-						'Client-Id': CLIENT_ID,
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify({
-						type: 'channel.chat.message',
-						version: '1',
-						condition: {
-							broadcaster_user_id: CHAT_CHANNEL_USER_ID,
-							user_id: CHAT_CHANNEL_USER_ID,
-						},
-						transport: {
-							method: 'websocket',
-							session_id: websocketSessionID
-						}
-					})
-				});
-		
-				if (response.status != 202) {
-					const data = await response.json();
-					console.error("Failed to subscribe to channel.chat.message. API call returned status code " + response.status);
-					console.error(data);
-					process.exit(1);
-				} else {
-					const data = await response.json();
-					console.log(`Subscribed to channel.chat.message [${data.data[0].id}]`);
-				}
-	}
+  {
+    // Register channel.chat.message
+    const response = await fetch(
+      "https://api.twitch.tv/helix/eventsub/subscriptions",
+      {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + OAUTH_TOKEN,
+          "Client-Id": CLIENT_ID,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          type: "channel.chat.message",
+          version: "1",
+          condition: {
+            broadcaster_user_id: CHAT_CHANNEL_USER_ID,
+            user_id: CHAT_CHANNEL_USER_ID,
+          },
+          transport: {
+            method: "websocket",
+            session_id: websocketSessionID,
+          },
+        }),
+      },
+    );
+
+    if (response.status != 202) {
+      const data = await response.json();
+      console.error(
+        "Failed to subscribe to channel.chat.message. API call returned status code " +
+          response.status,
+      );
+      console.error(data);
+      process.exit(1);
+    } else {
+      const data = await response.json();
+      console.log(`Subscribed to channel.chat.message [${data.data[0].id}]`);
+    }
+  }
 }
 
-
 async function rotateClockwise() {
-	await rotate(1);
+  await rotate(1);
 }
 
 async function rotateCounterClockwise() {
-	await rotate(-1);
+  await rotate(-1);
 }
