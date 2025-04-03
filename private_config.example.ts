cb4d83e9a7b331f9eef6196d3542228420021259
Takathedinosaur <takathedinosaur@gmail.com>
2025-04-03 20:50:29 +0200

    twitch chat bot that has the magical ability to rotate the stream & reset the stream state

---
+++ private_config.example.ts
@@ -0,0 +1,22 @@
+const config = {
+    twitch: {
+        client_id: "___",
+        client_secret: "___",
+        client_username: "___",
+        access_token: "___",
+        channel_id: "___",
+        channel_name: "___",
+        clockwise_event: "___",
+        counterclockwise_event: "___"
+    },
+    obs: {
+        ip: "___",
+        password: "___",
+        port: "___",
+        scene_name: "___"
+    }
+}
+
+
+export const twitch = config.twitch;
+export const obs = config.obs;
